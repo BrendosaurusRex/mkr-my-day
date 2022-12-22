a37efa538e0b80b7c0c902971ac5b49773e850ed
Brendon Barrios <40436651+BrendosaurusRex@users.noreply.github.com>
2022-12-21 22:25:35 -0800

    changes program to use I2S protocol instead of analog out

--- mkr-my-day.ino
+++ mkr-my-day.ino
@@ -1,48 +1,72 @@
-// MKR-My-Day! Rebel against Cancel Culture!
-
 #include <SD.h>
-#include <AudioZero.h>
+#include <ArduinoSound.h>
 
-const char filename[] = "jpiper~1.wav";
-const int  sampleRate = 2*44100;
+const char filename[] = "tone440.wav";
 
-File waveFile;
+SDWaveFile waveFile;
 
 void setup() {
 
-  Serial.begin(9600);
-  // while (!Serial);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(3000);
-  digitalWrite(LED_BUILTIN, LOW);
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(3000);
+    digitalWrite(LED_BUILTIN, LOW);
 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin()) {
-    Serial.println("Initialization failed!");
-    while(1);
-  }
-  Serial.println("Initialization done.");
+    Serial.print("Initializing SD card...");
+    if (!SD.begin()) {
+        Serial.println("Initialization failed!");
+        while(1);
+    }
+    Serial.println("Initialization done.");
 
-  waveFile = SD.open(filename);
-  if (!waveFile) {
-    Serial.println("Wave file is invalid!");
-    while(1); // do nothing
-  }
-  Serial.print("Loaded file: ");
-  Serial.println(filename);
+    waveFile = SDWaveFile(filename);
+    if (!waveFile) {
+        Serial.println("Wave file is invalid!");
+        while(1);
+    }
+    Serial.print("Loaded file: ");
+    Serial.println(filename);
 
-  AudioZero.begin(sampleRate);
-  Serial.println("Starting playback");
-  AudioZero.play(waveFile);
-  AudioZero.end();
-  Serial.println("Playback stopped");
+    Serial.print("Bits per sample = ");
+    Serial.println(waveFile.bitsPerSample());
+
+    long channels = waveFile.channels();
+    Serial.print("Channels = ");
+    Serial.println(channels);
+
+    long sampleRate = waveFile.sampleRate();
+    Serial.print("Sample rate = ");
+    Serial.print(sampleRate);
+    Serial.println(" Hz");
+
+    long duration = waveFile.duration();
+    Serial.print("Duration = ");
+    Serial.print(duration);
+    Serial.println(" seconds");
+
+    // adjust the playback volume
+    AudioOutI2S.volume(5);
+
+    // check if the I2S output can play the wave file
+    if (!AudioOutI2S.canPlay(waveFile)) {
+        Serial.println("unable to play wave file using I2S!");
+        while (1);
+    }
+
+    // start playback
+    Serial.println("Starting playback");
+    AudioOutI2S.play(waveFile);
+    Serial.println("Finished playback");
+    delay(1000);
 
 } // setup()
 
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    Serial.println("Starting playback");
+    AudioOutI2S.play(waveFile);
+    Serial.println("Finished playback");
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
 
