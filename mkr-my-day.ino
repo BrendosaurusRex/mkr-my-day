53ab941e61048bd04d0bdb906aa24305c9e2604e
Brendon Barrios <40436651+BrendosaurusRex@users.noreply.github.com>
2022-12-24 10:21:59 -0800

    attaches an interrupt for push-button playback

--- mkr-my-day.ino
+++ mkr-my-day.ino
@@ -3,6 +3,8 @@
 #include <SD.h>
 #include <AudioZero.h>
 
+#define PUSH_BUTTON 7
+
 const char filename[] = "jpiper~1.wav";
 const int  sampleRate = 2*44100;
 
@@ -10,33 +12,36 @@ File waveFile;
 
 void setup() {
 
-  Serial.begin(9600);
-  // while (!Serial);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(3000);
-  digitalWrite(LED_BUILTIN, LOW);
+    pinMode(PUSH_BUTTON, INPUT);
+    attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON), ISR, FALLING);
 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin()) {
-    Serial.println("Initialization failed!");
-    while(1);
-  }
-  Serial.println("Initialization done.");
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  waveFile = SD.open(filename);
-  if (!waveFile) {
-    Serial.println("Wave file is invalid!");
-    while(1); // do nothing
-  }
-  Serial.print("Loaded file: ");
-  Serial.println(filename);
+    Serial.begin(9600);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(3000);
+    digitalWrite(LED_BUILTIN, LOW);
 
-  AudioZero.begin(sampleRate);
-  Serial.println("Starting playback");
-  AudioZero.play(waveFile);
-  AudioZero.end();
-  Serial.println("Playback stopped");
+    Serial.print("Initializing SD card...");
+    if (!SD.begin()) {
+        Serial.println("Initialization failed!");
+        while(1);
+    }
+    Serial.println("Initialization done.");
+
+    waveFile = SD.open(filename);
+    if (!waveFile) {
+        Serial.println("Wave file is invalid!");
+        while(1); // do nothing
+    }
+    Serial.print("Loaded file: ");
+    Serial.println(filename);
+
+    AudioZero.begin(sampleRate);
+    Serial.println("Starting playback");
+    AudioZero.play(waveFile);
+    AudioZero.end();
+    Serial.println("Playback stopped");
 
 } // setup()
 
@@ -47,4 +52,13 @@ void loop() {
     delay(1000);
 
 } // loop()
+
+void ISR() {
+
+    Serial.println("Starting playback");
+    AudioZero.play(waveFile);
+    AudioZero.end();
+    Serial.println("Playback stopped");
+
+} // ISR()
 
